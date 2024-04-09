8debda277b3c905488b3b05670ca7cd2e5a2bac1
Gabriel Lopes <165069789+gabriellopes061@users.noreply.github.com>
2024-04-09 09:56:30 -0300

    Add files via upload

---
+++ Lógica de programação e algoritmos/Python/atividade 14 - verificar ano.py
@@ -0,0 +1,28 @@
+ano = int(input('Digite um numero para o mês: '))
+
+if ano == 1:
+    print('O mês é janeiro')
+elif ano == 2:
+    print('O mês é fevereiro')
+elif ano == 3:
+    print('O mês é março')
+elif ano == 4:
+    print('O mês é abril')
+elif ano == 5:
+    print('O mês é maio')
+elif ano == 6:
+    print('O mês é junho')
+elif ano == 7:
+    print('O mês é julho')
+elif ano == 8:
+    print('O mês é agosto')
+elif ano == 9:
+    print('O mês é stembro')
+elif ano == 10:
+    print('O mês é outubro')
+elif ano == 11:
+    print('O mês é novembro')
+elif ano == 12:
+    print('O mês é dezembro')
+else:
+    print('digite um mês válido')
