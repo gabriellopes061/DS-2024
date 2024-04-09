84176a8fd5acb4d857cf80efb8405dc31dee0b87
Gabriel Lopes <165069789+gabriellopes061@users.noreply.github.com>
2024-04-09 09:55:00 -0300

    Add files via upload

---
+++ Lógica de programação e algoritmos/Python/atividade 01 - media.py
@@ -0,0 +1,17 @@
+n1 = float(input("digite a primeira nota: "))
+
+n2 = float(input("digite a segunda nota: "))
+
+n3 = float(input("digite a terceira nota: "))
+
+soma = n1 + n2 + n3
+media = soma / 3
+
+print(f"a média do aluno foi: {round(media, 2)}")
+
+if media >= 7:
+    print("Aprovado")
+elif media >= 3:
+    print("Recuperação")
+else:
+    print("Reprovado")
