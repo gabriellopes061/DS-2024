1c9a35e0edfdea0e7c65d36f1e0de20efc2781b6
Gabriel Lopes <165069789+gabriellopes061@users.noreply.github.com>
2024-04-09 11:51:49 -0300

    Add files via upload

---
+++ Lógica de programação e algoritmos/Python/atividade 24 - listas.py
@@ -0,0 +1,18 @@
+"""
+lista = []
+
+lista.append("v1")
+lista.append("v2")
+lista.append("v3")
+print(lista)
+"""
+
+frutas = []
+frutas.append("maçã")
+frutas.append("bergamota")
+frutas.append("banana")
+frutas.append("melão")
+frutas.append("laranja")
+print(frutas)
+frutas.pop(0)
+#print(f'frutas selecionadas: {frutas[0]} e {frutas[2]}')
---
+++ Lógica de programação e algoritmos/Python/atividades 25 - DEF.py
@@ -0,0 +1,27 @@
+def soma(n1,n2):
+    return (n1+n2)
+
+print(soma(1,1))
+a = soma(10,20)
+print(a)
+
+def multiplicacao(n1,n2):
+    return (n1*n2)
+
+print(multiplicacao(10,5))
+b = multiplicacao(5,5)
+print(b)
+
+def subtracao(n1,n2):
+    return (n1-n2)
+
+print(subtracao(1,1))
+c = subtracao(10,5)
+print(c)
+
+def divisao(n1,n2):
+    return (n1/n2)
+
+print(divisao(10,5))
+d = divisao(25,5)
+print(d)
